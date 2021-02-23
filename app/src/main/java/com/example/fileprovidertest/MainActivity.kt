6633a5d9f1543a8574ea6376820bbea4adbbbfa6
2017398956 <2017398956@qq.com>
2021-02-23 17:08:36 +0800

    update

--- app/src/main/java/com/example/fileprovidertest/MainActivity.kt
+++ app/src/main/java/com/example/fileprovidertest/MainActivity.kt
@@ -11,6 +11,7 @@ import android.os.StrictMode
 import android.os.StrictMode.VmPolicy
 import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
